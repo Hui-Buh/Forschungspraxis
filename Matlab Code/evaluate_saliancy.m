c7bd24e396044d60814a173e5a2144f34c949b3e
Hui-Buh <heinzuwe.Kaschperl@web.de>
2016-04-18 17:14:36 +0200

    Erste Saliancy dabei - Grauwerte am Sakkadenendpunkt

--- Matlab Code/evaluate_saliancy.m
+++ Matlab Code/evaluate_saliancy.m
@@ -34,12 +34,9 @@ function evaluate_saliancy(bilder, durchlauf, saliancy, kontroll_kennung, patien
                 image = rgb2gray(image);
                 image = round(image);
                 if strcmp(saliancy, 'Gray scale at saccade end point') == 1
-                    Sakk_pos = center_image(Sakk_parsed(:,5), Sakk_parsed(:,5), '-', '-', '-', '-', 300, 460);
+                    Sakk_pos = center_image(Sakk_parsed(:,5), Sakk_parsed(:,6), '-', '-', '-', '-', size(image, 2), size(image, 1));
                     data_control = [data_control; diag(image(floor(Sakk_pos(:,2)), floor(Sakk_pos(:,1))))];
                 end
-                
-                
-                
             end
         end
         if durchlauf == 2 || durchlauf == 3
@@ -51,15 +48,22 @@ function evaluate_saliancy(bilder, durchlauf, saliancy, kontroll_kennung, patien
                     continue
                 end
                 % Extrahiere alle notwendigen Daten
-                
-                
-                
+                clearvars Sakk_parsed Sakk_pos 
+                Sakk_parsed = m.Sakk_parsed;
+                image = imread(cat(2, image_path, '/', image_list{c,1}, '.jpg'));
+                image = rgb2gray(image);
+                image = round(image);
+                if strcmp(saliancy, 'Gray scale at saccade end point') == 1
+                    Sakk_pos = center_image(Sakk_parsed(:,5), Sakk_parsed(:,6), '-', '-', '-', '-', size(image, 2), size(image, 1));
+                    data_control = [data_control; diag(image(floor(Sakk_pos(:,2)), floor(Sakk_pos(:,1))))];
+                end
             end
         end
     end
 
 %% Alle Daten der Patienten heraussuchen
 
+    data_patient = 0;
     
     for b = 2:size(patient_listing,1)
         if durchlauf == 1 || durchlauf == 3
@@ -71,8 +75,15 @@ function evaluate_saliancy(bilder, durchlauf, saliancy, kontroll_kennung, patien
                     continue
                 end
                 % Extrahiere alle notwendigen Daten
-                
-                
+                clearvars Sakk_parsed Sakk_pos 
+                Sakk_parsed = m.Sakk_parsed;
+                image = imread(cat(2, image_path, '/', image_list{c,1}, '.jpg'));
+                image = rgb2gray(image);
+                image = round(image);
+                if strcmp(saliancy, 'Gray scale at saccade end point') == 1
+                    Sakk_pos = center_image(Sakk_parsed(:,5), Sakk_parsed(:,6), '-', '-', '-', '-', size(image, 2), size(image, 1));
+                    data_patient = [data_patient; diag(image(floor(Sakk_pos(:,2)), floor(Sakk_pos(:,1))))];
+                end
             end
         end
         if durchlauf == 2 || durchlauf == 3
@@ -84,15 +95,54 @@ function evaluate_saliancy(bilder, durchlauf, saliancy, kontroll_kennung, patien
                     continue
                 end
                 % Extrahiere alle notwendigen Daten
-                
-                
+                clearvars Sakk_parsed Sakk_pos 
+                Sakk_parsed = m.Sakk_parsed;
+                image = imread(cat(2, image_path, '/', image_list{c,1}, '.jpg'));
+                image = rgb2gray(image);
+                image = round(image);
+                if strcmp(saliancy, 'Gray scale at saccade end point') == 1
+                    Sakk_pos = center_image(Sakk_parsed(:,5), Sakk_parsed(:,6), '-', '-', '-', '-', size(image, 2), size(image, 1));
+                    data_patient = [data_patient; diag(image(floor(Sakk_pos(:,2)), floor(Sakk_pos(:,1))))];
+                end
             end
         end
     end
 
 %% Auswertung
-   
-% roc
 
+    data_patient(1) = [];
+    data_control(1) = [];
+    
+    data_patient = double(data_patient');
+    data_control = double(data_control');
+    threshold = 1:1:255;
+    
+%     % classifier: everything smaller than a threshold is patient. 
+%     for a = 1:255
+%         TPR(a) = size(data_patient(data_patient < threshold(a))) / size(data_patient);
+%         FPR(a) = size(data_control(data_control < threshold(a))) / size(data_control);
+%     end
+    
+    % classifier: everything higer than a threshold is patient. 
+    for a = 1:255
+        TPR(a) = size(data_patient(data_patient > threshold(a))) / size(data_patient);
+        FPR(a) = size(data_control(data_control > threshold(a))) / size(data_control);
+    end
 
+    figure(1)
+    hold on; grid on;
+    histogram(data_control,100);
+    histogram(data_patient,100);
+    legend('Control', 'Patient')
+    xlabel('gray scale values')
+    ylabel('# saccades');
+    
+    figure(2)
+    hold on; grid on;
+    plot(FPR,TPR);
+    plot([0 1], [0 1],'k')
+    legend('ROC-curve');
+    xlabel('False Positive Rate (%)');
+    ylabel('True Positive Rate (%)');
+    
 end
